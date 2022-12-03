aeda6ea485c7b3c5ddc32dfac8f1c04dc1e43a5d
Marcus Lilja <marcus.t.lilja@gmail.com>
2022-12-03 20:50:05 +0100

    Drawing a thingy!

---
+++ RayTracing/src/Renderer.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <Walnut/Image.h>
+
+#include <memory>
+#include <glm/glm.hpp>
+
+class Renderer
+{
+public:
+
+	Renderer() = default;
+
+	void OnRisize(uint32_t width, uint32_t height);
+	void Render();
+
+	std::shared_ptr<Walnut::Image> GetFinalImage() const { return m_FinalImage; }
+
+private:
+
+	uint32_t PerPixel(const glm::vec2& coord);
+
+private:
+
+	std::shared_ptr<Walnut::Image> m_FinalImage;
+	uint32_t* m_ImageData = nullptr;
+};
+
